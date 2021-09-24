ed5093e7b8e6e2305626e2008242681bd3925c97
victorcav <victor.bcav@gmail.com>
2021-09-24 20:13:42 -0300

    Alterando o codigo de JS

--- app_financeiro/script.js
+++ app_financeiro/script.js
@@ -1,75 +1,93 @@
-// Pegar componentes por ID
-const transactionUl = document.querySelector('#transactions');
-const moneyPlusDisplay = document.querySelector('#money-plus');
-const moneyMinusDisplay = document.querySelector('#money-minus');
-const balanceDisplay = document.querySelector('#balance');
-const inputTransactionName = document.querySelector('#text');
-const inputTransactionAmount = document.querySelector('#amount');
+// Pegar componente por ID:
 
+const transactionsUl = document.querySelector("#transactions");
 
+const moneyPlusDisplay = document.querySelector("#money-plus");
 
-//Declaracao de um objeto literal
-const dummyTransactions = [
-    {id: 1, name: 'Bolo de brigadeiro', amount: -20},
-    {id: 2, name: 'Salario', amount: 30},
-    {id: 3, name: 'Torta de limao', amount: -20},
-    {id: 4, name: 'Violao', amount: 150}
-]
+const moneyMinusDisplay = document.querySelector("#money-minus");
 
-const addTransactionIntoDom = transaction => {
-    const operator = transaction.amount < 0 ? '-' : '+';
-    const amountWithoutOperator = Math.abs(transaction.amount);
-    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus';
-    const li = document.createElement('li');
-    li.classList.add(CSSClass);
-    li.innerHTML = `
-        ${transaction.name} <span> ${operator} R$ ${amountWithoutOperator} </span>
-         <button class= "delete-btn">x</button>`
-    transactionUl.append(li);
-}
+const balanceDisplay = document.querySelector("#balance");
 
-/*
-Metodo que ira retornar todos os valores somados de receitas e despesas 
-*/
+const inputTransactionName = document.querySelector("#text");
+
+const inputTransactionAmount = document.querySelector("#amount");
+
+// Declaraccao de um objeto literal:
+
+const dummyTransactions = [];
+const addTransactionIntoDom = (transaction) => {
+  const operator = transaction.amount < 0 ? "-" : "+"; // if ternario ou if linha
+  const amountWithoutOperator = Math.abs(transaction.amount);
+  const CSSClass = transaction.amount < 0 ? "minus" : "plus";
+  const li = document.createElement("li");
+  li.classList.add(CSSClass);
+  li.innerHTML = ` 
+            ${transaction.name} <span> ${operator} R$  ${amountWithoutOperator} </span> 
+                                    <button class="delete-btn">x</button> `;
+  transactionsUl.append(li);
+};
+
+/* Metodo que ira retornar todo os valores somados de receitas e despesas */
 
 const updateBalanceValues = () => {
-    
-    //Pegar todos os valores do objeto dummy
-    const transactionAmounts = dummyTransactions.map(transaction => transaction.amount);
+  // pegar todos os valores do objeto dummy
+  const transactionAmounts = dummyTransactions.map(
+    (transaction) => transaction.amount
+  );
 
-    // Somatorio dos valores retomados
-    const total = transactionAmounts
-                .reduce((acumulator, transaction) => acumulator + transaction, 0)
-                .toFixed(2);
+  // somatorio dos valores retornados
+  const total = transactionAmounts
+    .reduce((acumulator, transaction) => acumulator + transaction, 0)
+    .toFixed(2);
 
-    // Retorna todas as receitas
-    const income = transactionAmounts
-                    .filter(values => values > 0)
-                    .reduce((acumulator, values) => acumulator + values, 0)
-                    .toFixed(2);
+  // retorna todas as receitas
+  const income = transactionAmounts
+    .filter((values) => values > 0)
+    .reduce((acumulator, values) => acumulator + values, 0)
+    .toFixed(2);
 
+  // retorna todas as despesas
+  const expense = transactionAmounts
+    .filter((values) => values < 0)
+    .reduce((acumulator, values) => acumulator - values, 0)
+    .toFixed(2);
 
-    // Retorna todas as despesas
-    const expense = transactionAmounts
-                    .filter(values => values < 0)
-                    .reduce((acumulator, values) => acumulator - values, 0)
-                    .toFixed(2);
-
-    balanceDisplay.textContent = `R$ ${total}`
-    moneyPlusDisplay.textContent = `R$ ${income}`
-    moneyMinusDisplay.textContent = `R$ ${expense}`
-
-
-}
-
+  balanceDisplay.textContent = `R$ ${total}`;
+  moneyPlusDisplay.textContent = `R$ ${income}`;
+  moneyMinusDisplay.textContent = `R$ ${expense}`;
+};
 const init = () => {
-    dummyTransactions.forEach(addTransactionIntoDom);
-    updateBalanceValues();
-}
+  // inicializa todas as funções da pagina
+  transactionsUl.innerHTML = "";
+  dummyTransactions.forEach(addTransactionIntoDom);
+  updateBalanceValues();
+};
+
 init();
+//  funcao que gera ID
+const generateId = () => Math.round(Math.random() * 1000);
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const transactionName = inputTransactionName.value.trim();
+  const transactionAmout = inputTransactionAmount.value.trim();
 
-// <li class="plus">
-// Salário <span>$400</span><button class="delete-btn">x</button>
-// </li>
+  if (transactionName === "" && transactionAmout === "") {
+    alert("Informe os campos valor e nome da transação");
+    return;
+  }
+
+  const transaction = {
+    id: generateId(),
+    name: transactionName,
+    amount: transactionAmout,
+  };
+
+  dummyTransactions.push(transaction);
+  init();
+  updateLocalStorage;
+
+  transactionName.value = "";
+  transactionAmout.value = "";
+});
